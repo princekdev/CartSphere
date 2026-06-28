2486ed99acd32f08a1a61b8590135f5012827f98
PRINCE123853 <princekumar2005itaha@gmail.com>
2026-06-28 17:12:09 +0530

    Register cart routes

---
+++ backend/routes/cartRoutes.js
@@ -0,0 +1,12 @@
+// cartRoutes.js
+const express = require('express');
+const { getCart, addToCart, updateCartItem, removeFromCart, clearCart } = require('../controllers/cartController');
+const { protect } = require('../middleware/authMiddleware');
+const router = express.Router();
+router.use(protect);
+router.get('/', getCart);
+router.post('/', addToCart);
+router.put('/:itemId', updateCartItem);
+router.delete('/:itemId', removeFromCart);
+router.delete('/', clearCart);
+module.exports = router;
